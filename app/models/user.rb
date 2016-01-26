bd60313f082a4522024c75ce3c99868c92d5917c
App Academy Student <appacademy@Apps-Mac-mini-85.local>
2016-01-26 17:27:28 -0500

    Add button to log in as guest; add hover actions to log in buttons

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,7 @@ class User < ActiveRecord::Base
 
   after_initialize :ensure_session_token
 
-  has_many (
-    :articles,
-    class_name: "Article",
-    foreign_key: :author_id,
-    primary_key: :id
-  )
+  has_many :articles, class_name: "Article", foreign_key: :author_id
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
