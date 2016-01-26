833579a3ba0b94f5fff6ece879e78abcca61fa52
App Academy Student <appacademy@Apps-Mac-mini-85.local>
2016-01-26 15:43:33 -0500

    write Api::ArticlesController

---
+++ app/controllers/api/articles_controller.rb
@@ -0,0 +1,40 @@
+class Api::ArticlesController < ApplicationController
+
+  def index
+    @top_articles = Article.whats_new
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      render :show
+    else
+      render json: @article.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @article = Article.find_by_id(params[:id])
+  end
+
+  def update
+    @article = Article.find_by_id(params[:id])
+
+    if @article.update
+      render :show
+    else
+      render json: @article.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body, :author_id)
+  end
+
+end
